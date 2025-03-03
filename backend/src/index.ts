26c68f0f53b5c933c80680a5a54cd82838cddffc
Sakuni1004 <sakunikakarunathilaka99@gmail.com>
2025-03-03 11:03:29 +0530

    connect database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from './config/database';
 
 dotenv.config();
 
@@ -7,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+connectDB();
 
 // Test route
 app.get("/", (req, res) => {
